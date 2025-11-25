179c5e6b305a5de1246726019980756ef5012078
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2025-11-25 14:02:51 -0500

    correcion para despliegue

--- config.py
+++ config.py
@@ -5,14 +5,19 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'jwt_secret'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_secret_key'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
+
+    database_url = os.environ.get(
+        'DATABASE_URL', 
         'postgresql+psycopg2://postgres:Sebastianalonso19@localhost:5432/asistencia_iot'
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get('DEBUG', 'False') == 'True'
     PROPAGATE_EXCEPTIONS = True
-
     JWT_ALGORITHM = 'HS256'  
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1) 
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
