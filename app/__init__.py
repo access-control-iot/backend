895284b781e85364a1752e0b3b7eeafb56cee657
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2025-11-26 21:27:59 -0500

    correciones despliegue con cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from flask_cors import CORS  
 from config import Config
 
 db = SQLAlchemy()
@@ -16,14 +17,15 @@ def create_app():
     jwt.init_app(app)
     migrate.init_app(app, db)
 
-    # IMPORTAR BLUEPRINTS
+
+    CORS(app) 
     from app.routes.auth import bp as auth_bp
     from app.routes.access import bp as access_bp
     from app.routes.attendance import bp as attendance_bp
     from app.routes.user import user_bp
     from app.routes.schedule import schedule_bp   
 
-  
+
     app.register_blueprint(auth_bp)  
     app.register_blueprint(access_bp, url_prefix='/access')  
     app.register_blueprint(attendance_bp)
