2ed6b6e251f285c2b22c35f859324b2efd70e831
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2025-11-28 01:06:08 -0500

    correcion -filtros y accesov8

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'jwt_secret'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_secret_key'
 
-
     database_url = os.environ.get(
         'DATABASE_URL', 
         'postgresql+psycopg2://postgres:Sebastianalonso19@localhost:5432/asistencia_iot'
@@ -14,7 +13,8 @@ class Config:
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = database_url
+  
+    SQLALCHEMY_DATABASE_URI = database_url + "?timezone=America/Lima"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get('DEBUG', 'False') == 'True'
